e68c85f0328b9b397c8d2b2ca0d1a0aa8b93f83f
ronniedada <ronniedada@gmail.com>
2012-09-12 18:26:05 -0700

    supported table footers
    Footers are used to display useful infos such as
    timestamp, prog state, etc

--- tabula/table.py
+++ tabula/table.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-import logging
-import numpy as np
+import re
+import time
+
 from section import Section
 
 class Table(object):
@@ -16,8 +17,9 @@ class Table(object):
         ---------
         section-3
     """
-    def __init__(self, name="", width=800, height=600, sep="-"):
+    def __init__(self, name="", ftr="", width=800, height=600, sep="-"):
         self.name = name
+        self.ftr = ftr
         self.width = width          # max width
         self.height = height        # max height
         self.sep = sep
@@ -25,10 +27,10 @@ class Table(object):
         self.hori = True
 
     def __str__(self):
-        div = "\n%s\n" % (self.sep * self.size()[0])
+        div = "%s\n" % (self.sep * self.size()[0])
         return div.join(map(lambda x: str(x),
-                        sorted(self.sections.itervalues(),
-                               key=lambda y: y.id)))
+                            sorted(self.sections.itervalues(),
+                                   key=lambda y: y.id)) + [self.get_ftr()])
 
     def __repr__(self):
         return "Table(name=%r, sections=%r, hori=%r)"\
@@ -43,6 +45,26 @@ class Table(object):
             map(lambda x: x.size()[1], self.sections.itervalues()))
 
         return width, height
+
+    def set_ftr(self, ftr):
+        """
+        Set footer for this table
+        Accept the following regexps:
+
+        %time : current time printed as "hh:mm:ss"
+        """
+        self.ftr = ftr
+
+    def get_ftr(self):
+        """
+        Process footer and return the processed string
+        """
+        if not self.ftr:
+            return self.ftr
+
+        width = self.size()[0]
+        return re.sub(
+            "%time", "%s\n" % time.strftime("%H:%M:%S"), self.ftr).rjust(width)
 
     def get_section(self, name):
         if name in self.sections.iterkeys():
